84498c0297dff50e2f38b1513e9c99c6be54337b
Tom_gxz <75934344+tomgxz@users.noreply.github.com>
2024-10-31 01:35:58 +0000

    Fixed .heif processing and added run feedback
    Switched to imageio because pillow-heif wasn't working

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import json, os, logging, pillow_heif, sys, datetime
 from datetime import datetime
+START_TIME = datetime.now()
+images_start_time = None
+images_end_time = None
+
+import json, os, logging, sys, humanize
+import numpy as np
+import imageio.v2 as imageio
 from PIL import Image
 from PIL.ExifTags import TAGS, GPSTAGS
 
@@ -21,16 +27,19 @@ logging.basicConfig(
 
 LOGGER = logging.getLogger()
 
-pillow_heif.register_heif_opener() # Register HEIF support with Pillow
+feedback = {"total": 0,"processed": 0,"has_exif": 0,"has_timestamp": 0,"has_gps": 0,"failed": 0}
 
-
-def get_exif_data(image_path:str) -> dict | None:
+def get_exif_data(image_path:str,heic:bool=False) -> dict | None:
     try:
-        image = Image.open(image_path)
+        if heic:
+            image_data:np.ndarray = imageio.imread(image_path)
+            image = Image.fromarray(image_data)
+            exif_data = image.getexif()
+        else:
+            image = Image.open(image_path)
+            exif_data = image._getexif()
         
-        exif_data = image._getexif()
-        
-        if not exif_data: 
+        if exif_data == None: 
             LOGGER.warning(f"Error reading {image_path}: No exif data found in file")
             return None
         
@@ -42,12 +51,12 @@ def get_exif_data(image_path:str) -> dict | None:
         return None
 
 
-def convert_to_degrees(value):
+def convert_to_degrees(value: int|float) -> float:
     d, m, s = value
     return d + (m / 60.0) + (s / 3600.0)
 
 
-def get_lat_lon(gps_info):
+def get_lat_lon(gps_info: dict) -> tuple[float]:
     if not gps_info: return None, None
 
     lat, lon = None, None
@@ -60,7 +69,7 @@ def get_lat_lon(gps_info):
     return lat, lon
 
 
-def format_timestamp(timestamp_str):
+def format_timestamp(timestamp_str: str) -> str:
     if not timestamp_str: return
     
     try:
@@ -71,52 +80,72 @@ def format_timestamp(timestamp_str):
         return None
 
 
-def extract_metadata(photo_dir):
+def count_files(directory) -> int:
+    total_files = 0
+    for _, _, filenames in os.walk(directory): total_files += len(filenames)
+    return total_files
+
+
+def extract_metadata(photo_dir: str) -> list[dict]:
+    global images_start_time, images_end_time
+    
     data = []
     LOGGER.info(f"Starting metadata extraction from photos in {photo_dir}")
-
+    
+    total_files = count_files(photo_dir)
+    feedback["total"] = total_files
+    idx = 0 # counter for console feedback every 100 files, can't use enumerate because of the nested for loops
+    
+    images_start_time = datetime.now()
+    
     for root, _, files in os.walk(photo_dir):
-        for idx, file in enumerate(files):
+        for file in files:
             file_path = os.path.join(root, file)
+            idx += 1 # increment counter
             
-            if idx % 100 == 0:
-                s = f"Processing file {idx}/{len(files)} in current directory: {file_path}"
+            if idx % 100 == 0 or idx == 1:
+                s = f"Processing file {idx}/{total_files} in current directory: {file_path}"
                 print(s); LOGGER.info(s)
             
             if file.lower().endswith(('.jpg', '.jpeg', '.png', '.heic')) and not file.startswith("._"):
+                feedback["processed"]+=1
                 filename = file_path.split('/')[-1]
                 
-                exif = get_exif_data(file_path)
+                exif = get_exif_data(file_path,file.lower().endswith(('.heic')))
                 
                 if not exif:
                     data.append({"filename": filename, "error": "no exif data"})
                     continue
-                    
-                gps_info = exif.get("GPSInfo") # TODO: property doesn't exist?
+                
+                feedback["has_exif"]+=1
+                
+                gps_info: dict|None = exif.get("GPSInfo") # TODO: property doesn't exist?
                 timestamp = exif.get("DateTimeOriginal") or exif.get("DateTime")
                 
                 lat, lon = get_lat_lon(gps_info)
                 timestamp = format_timestamp(timestamp)
-
+                
                 entry = {"filename": filename}
                 
-                if timestamp: entry["timestamp"] = timestamp
+                if timestamp: entry["timestamp"] = timestamp; feedback["has_timestamp"]+=1
                 else: entry["error"] = "missing data: time"
                 
-                if lat: entry["latitude"] = lat
-                else: entry["error"] = "missing data: gps"
+                if lat and lon:
+                    entry["latitude"] = lat
+                    entry["longitude"] = lon
+                    feedback["has_gps"]+=1
+                else: entry["error"] = "missing data: gps"; continue
                 
-                if lon: entry["longitude"] = lon
-                else: entry["error"] = "missing data: gps"
+                if entry == {}: entry["error"] = "missing data"; continue
                 
-                if entry == {}: entry["error"] = "missing data"
-                    
                 data.append(entry)
+
+    images_end_time = datetime.now()
 
     return data
 
 # Set path to your local photo directory
-PHOTO_DIR = "/Volumes/EXTERNAL2/Images/CAMERA ROLL"
+PHOTO_DIR = "/Volumes/EXTERNAL2/Images/CAMERA ROLL/"
 OUTPUT_FILE = "./output/photo_metadata.json"
 
 metadata = extract_metadata(PHOTO_DIR)
@@ -125,4 +154,9 @@ with open(OUTPUT_FILE, "w") as f:
     json.dump(metadata, f, indent=4)
     LOGGER.info(f"Metadata saved to {OUTPUT_FILE}")
 
-print(f"Metadata saved to {OUTPUT_FILE}")
+END_TIME = datetime.now()
+DURATION = END_TIME - START_TIME
+image_duration = images_end_time - images_start_time
+
+print(f"------------------------\nMetadata saved to {OUTPUT_FILE}\nCompleted in {humanize.precisedelta(DURATION, minimum_unit="microseconds", suppress=["days"])}\nAverage time per image: {humanize.precisedelta(image_duration/feedback['processed'], minimum_unit="microseconds", suppress=["days"])}")
+print(f"------------------------\nTotal files in directory: {feedback['total']}\nImages processed: {feedback['processed']}\nImages without exif data: {feedback['processed'] - feedback['has_exif']}\nImages without gps: {feedback['processed'] - feedback['has_gps']}\nFailed: {feedback['failed']}")
